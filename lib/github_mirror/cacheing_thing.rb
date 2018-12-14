6dbb90385b28faf121b57e8c2b9bc32d995503ca
Rachel Evans <git@rve.org.uk>
2018-12-14 17:59:12 +0000

    Fix CacheingThing bug on fresh file; namespace into GithubMirror class

--- lib/github_mirror/cacheing_thing.rb
+++ lib/github_mirror/cacheing_thing.rb
@@ -1,59 +1,60 @@
 require 'json'
 
-class CacheingThing
-  def initialize(filename, cutoff_time, &block)
-    @filename = filename
-    @cutoff_time = cutoff_time
-    @block = block
-  end
-
-  def each
-    raise unless block_given?
-
-    return if with_fresh_file do |f|
-      JSON.load(f).each do |v|
-        yield v
-      end
+class GithubMirror
+  class CacheingThing
+    def initialize(filename, cutoff_time, &block)
+      @filename = filename
+      @cutoff_time = cutoff_time
+      @block = block
     end
 
-    tmp = @filename + ".tmp"
-    File.open(tmp, 'w') do |f|
-      f.print '['
-      e = @block.call
-      i = 0
-      e.each do |v|
-        f.print ',' unless i == 0
-        i += 1
-        JSON.dump(v, f)
-        yield v.to_h
+    def each
+      raise unless block_given?
+
+      return if with_fresh_file do |f|
+        JSON.load(f).each do |v|
+          yield v
+        end
       end
-      f.print "]\n"
-    end
-    File.rename tmp, @filename
 
-    nil
-  end
+      tmp = @filename + ".tmp"
+      File.open(tmp, 'w') do |f|
+        f.print '['
+        e = @block.call
+        i = 0
+        e.each do |v|
+          f.print ',' unless i == 0
+          i += 1
+          JSON.dump(v, f)
+          yield v.to_h
+        end
+        f.print "]\n"
+      end
+      File.rename tmp, @filename
 
-  private
-
-  def with_fresh_file
-    f = begin
-      File.open(@filename, 'r')
-    rescue Errno::ENOENT
       nil
     end
 
-    f or return
+    private
 
-    begin
-      if f.stat.mtime > @cutoff_time
-        yield f
+    def with_fresh_file
+      f = begin
+        File.open(@filename, 'r')
+      rescue Errno::ENOENT
+        nil
       end
-    ensure
-      f.close
+
+      begin
+        if f and f.stat.mtime > @cutoff_time
+          yield f
+          true
+        else
+          false
+        end
+      ensure
+        f.close if f
+      end
     end
 
-    true
   end
-
 end
--- lib/github_mirror/github_paginating_enumerator.rb
+++ lib/github_mirror/github_paginating_enumerator.rb
@@ -11,65 +11,64 @@ class Github::ResponseWrapper
   end
 end
 
-class GithubPaginatingEnumerator
+class GithubMirror
+  class GithubPaginatingEnumerator
 
-  include Enumerable
+    include Enumerable
 
-  def initialize(first_page)
-    @page = first_page
-    @page_number = 0
-    @index = 0
-  end
-  
-  def each
-    return self unless block_given?
-
-    stopped = false
-    while true
-      r = begin
-        self.next
-      rescue StopIteration
-        stopped = true
-      end
-
-      break if stopped
-      yield r
-    end
-  end
-
-  def next
-    raise if block_given?
-    # puts "#{inspect}#next called"
-
-    loop do
-      if @index < @page.count
-        v = @page[@index]
-        @index += 1
-        # puts "Returning #{v.class}"
-        return v
-      end
-
-      unless @page.has_next_page?
-        break
-      end
-
-      @page = @page.next_page
-      @page_number += 1
+    def initialize(first_page)
+      @page = first_page
+      @page_number = 0
       @index = 0
-      puts "Now self=#{inspect}"
     end
 
-    # puts "raising StopIteration"
-    raise StopIteration.new
-  end
+    def each
+      return self unless block_given?
 
-  def inspect
-    "#<#{self.class}:0x%016x %d/%d %d %s>" % [
-      object_id,
-      @index,
-      @page.count,
-      @page_number,
-      @page.has_next_page?,
-    ]
+      stopped = false
+      while true
+        r = begin
+          self.next
+        rescue StopIteration
+          stopped = true
+        end
+
+        break if stopped
+        yield r
+      end
+    end
+
+    def next
+      raise if block_given?
+      # puts "#{inspect}#next called"
+
+      loop do
+        if @index < @page.count
+          v = @page[@index]
+          @index += 1
+          return v
+        end
+
+        unless @page.has_next_page?
+          break
+        end
+
+        @page = @page.next_page
+        @page_number += 1
+        @index = 0
+      end
+
+      raise StopIteration.new
+    end
+
+    def inspect
+      "#<#{self.class}:0x%016x %d/%d %d %s>" % [
+        object_id,
+        @index,
+        @page.count,
+        @page_number,
+        @page.has_next_page?,
+      ]
+    end
   end
 end
